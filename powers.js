b90daa2902851559cf560f12b0483ae785e05b04
Zach Nicholson <raiste.doctow@gmail.com>
2020-04-26 20:40:04 -0500

    consolidated activate functions into one

--- powers.js
+++ powers.js
@@ -1,14 +1,4 @@
-document.querySelector("#activate-flight").addEventListener('click', flightHandlerFunction = () => {
-    document.querySelector('#flight').classList.replace('disabled', 'enabled');
-})
-
-document.querySelector("#activate-mindreading").addEventListener('click', mindreadingHandlerFunction = () => {
-    document.querySelector('#mindreading').classList.replace('disabled', 'enabled');
-})
-
-document.querySelector("#activate-xray").addEventListener('click', xrayHandlerFunction = () => {
-    document.querySelector('#xray').classList.replace('disabled', 'enabled');
-})
+// two buttons to either active or deactivate all powers
 
 const powers = document.querySelectorAll('.power')
 document.querySelector('#activate-all').addEventListener('click', activateallHandlerFunction = () => {
@@ -22,3 +12,16 @@ document.querySelector('#deactivate-all').addEventListener('click', deactivateal
         powers[i].classList.replace('enabled', 'disabled');
     }
 })
+
+/* You may notice that your code to enable individual powers (not all at once) is very similar. To keep your code DRY, make one function that will handle activating a power depending on which button is clicked. (Hint: one way to get started is to use event.target.id.split("-") in your function) */
+
+// if id includes 'activate' and doesn't include 'all', adds click event listener to button, replaces class of disabled with enabled
+
+document.querySelectorAll('button').forEach(button => {
+    const splitIdArr = button.id.split('-');
+    if (splitIdArr[0] == 'activate' && splitIdArr[1] !== 'all') {
+        button.addEventListener('click', func = () => {
+            document.getElementById(splitIdArr[1]).classList.replace('disabled', 'enabled');
+        })
+    }; 
+});
